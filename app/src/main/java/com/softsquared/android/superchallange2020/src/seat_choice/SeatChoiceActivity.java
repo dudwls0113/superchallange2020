2ff3a466e855a089d5d7bb024ee529531579e560
Arkinee <smj7162@naver.com>
2020-01-17 04:40:28 +0900

    좌석 선택 디자인 중

--- app/src/main/java/com/softsquared/android/superchallange2020/src/seat_choice/SeatChoiceActivity.java
+++ app/src/main/java/com/softsquared/android/superchallange2020/src/seat_choice/SeatChoiceActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import com.softsquared.android.superchallange2020.R;
@@ -19,6 +20,9 @@ import org.json.JSONException;
 import java.util.List;
 
 public class SeatChoiceActivity extends BaseActivity implements ChoiceActivityView {
+
+    ImageView mIvSeat1;
+    ImageView mIvSeat2;
 
     Spinner spinner;
     Context mContext;
@@ -33,6 +37,9 @@ public class SeatChoiceActivity extends BaseActivity implements ChoiceActivityVi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat_choice);
         mContext = this;
+
+        mIvSeat1 = findViewById(R.id.activity_seat_choice_iv_seat1);
+        mIvSeat2 = findViewById(R.id.activity_seat_choice_iv_seat4);
 
         spinner = findViewById(R.id.activity_seat_choice_spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(mContext, R.layout.custom_spinner, strings);
@@ -74,14 +81,27 @@ public class SeatChoiceActivity extends BaseActivity implements ChoiceActivityVi
                     showCustomToast("이 좌석은 예약할 수 없습니다.");
                 }
                 break;
-            case R.id.activity_seat_choice_iv_seat2:
-
-                break;
-            case R.id.activity_seat_choice_iv_seat3:
-
-                break;
             case R.id.activity_seat_choice_iv_seat4:
+                if (seat1 == 0) {
+                    ChoiceDialog choiceDialog = new ChoiceDialog(mContext, new ChoiceDialog.CustomLIstener() {
+                        @Override
+                        public void yesClick() {
+                            try {
+                                reservationRequest(2);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
 
+                        @Override
+                        public void noClick() {
+
+                        }
+                    });
+                    choiceDialog.show();
+                } else {
+                    showCustomToast("이 좌석은 예약할 수 없습니다.");
+                }
                 break;
 
             default:
@@ -101,23 +121,31 @@ public class SeatChoiceActivity extends BaseActivity implements ChoiceActivityVi
         // 0: 사람 없음 1: 사람 있음 2: 임산부가 앉아 있음 3: 예약됨
         switch (seatA.getStatus()) {
             case 0:
+                mIvSeat1.setImageDrawable(getDrawable(R.drawable.ic_status_0));
                 break;
             case 1:
+                mIvSeat1.setImageDrawable(getDrawable(R.drawable.ic_status_1));
                 break;
             case 2:
+                mIvSeat1.setImageDrawable(getDrawable(R.drawable.ic_status_2));
                 break;
             case 3:
+                mIvSeat1.setImageDrawable(getDrawable(R.drawable.ic_status_3));
                 break;
         }
 
         switch (seatB.getStatus()) {
             case 0:
+                mIvSeat2.setImageDrawable(getDrawable(R.drawable.ic_status_0));
                 break;
             case 1:
+                mIvSeat2.setImageDrawable(getDrawable(R.drawable.ic_status_1));
                 break;
             case 2:
+                mIvSeat2.setImageDrawable(getDrawable(R.drawable.ic_status_2));
                 break;
             case 3:
+                mIvSeat2.setImageDrawable(getDrawable(R.drawable.ic_status_3));
                 break;
         }
 
